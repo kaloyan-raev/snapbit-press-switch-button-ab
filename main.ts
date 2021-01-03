c3dd99b43fe419f17fd2975cb7ee6e1342984405
Kaloyan Raev <kaloyan-raev@users.noreply.github.com>
2021-01-03 19:27:30 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.East)
+})
